7bd3ca66a65bf2f00d81d57825f1224f5d100ab5
abdninesix <mohabd1995@gmail.com>
2025-07-29 16:25:24 +0500

    Updated

--- Backend/Task3/client/src/pages/Home.jsx
+++ Backend/Task3/client/src/pages/Home.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import Banner from '../components/Banner';
-import FileUpload from '../components/FileUpload';
 import FileList from '../components/FileList';
 
 const Dashboard = () => {
--- Backend/Task3/client/src/pages/Upload.jsx
+++ Backend/Task3/client/src/pages/Upload.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import Banner from '../components/Banner';
 import FileUpload from '../components/FileUpload';
-import FileList from '../components/FileList';
 
 const Upload = () => {
 
